6dcc9549a37eea80939bc1de6eb87c703495ae73
bystrovdev <bystrovdev@mail.ru>
2023-01-27 11:38:48 +0400

    Finished Android FragmentLifecycle app

--- app/src/main/java/ru/synergy/fragmentlifecycleexample/MainActivity.java
+++ app/src/main/java/ru/synergy/fragmentlifecycleexample/MainActivity.java
@@ -1,14 +1,61 @@
 package ru.synergy.fragmentlifecycleexample;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import ru.synergy.fragmentlifecycleexample.fragments.FirstFragment;
+import ru.synergy.fragmentlifecycleexample.fragments.SecondFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button fragmentButton1, fragmentButton2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragmentButton1 = (Button) findViewById(R.id.buttonFragment1);
+        fragmentButton2 = (Button) findViewById(R.id.buttonFragment2);
+
+        // Fragment specific
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        FirstFragment firstFragment = FirstFragment.newInstance("1", "2");
+        fragmentTransaction.add(R.id.container, firstFragment);
+        fragmentTransaction.commit();
+
+
+        fragmentButton1.setOnClickListener(onButtonClickListener);
+        fragmentButton2.setOnClickListener(onButtonClickListener);
     }
+    Button.OnClickListener onButtonClickListener = new Button.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            Fragment newFragment = null;
+
+            if(v == fragmentButton1){
+                newFragment = new FirstFragment();
+            } else{
+                newFragment = new SecondFragment();
+            }
+
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+            transaction.replace(R.id.container, newFragment);
+            transaction.addToBackStack(null);
+
+            transaction.commit();
+        }
+    };
 }
